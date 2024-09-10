52125413d9ca2d4d623a971614ef5f617d36bfbd
JeongHyeon Song <sjh00kr@naver.com>
2024-09-10 22:21:38 +0900

    Title: 합과 평균의 차, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240910/합과 평균의 차/sub-of-average-and-sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int sum = a+b+c;
+        int avg = sum/3;
+        System.out.println(sum);
+        System.out.println(avg);
+        System.out.println(sum-avg);
+    }
+}

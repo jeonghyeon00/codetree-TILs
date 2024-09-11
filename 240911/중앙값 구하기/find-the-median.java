b633089460d7e12a909eb13b24cbd0c64d01fcf5
JeongHyeon Song <sjh00kr@naver.com>
2024-09-11 13:57:09 +0900

    Title: 중앙값 구하기, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

--- 240911/중앙값 구하기/find-the-median.java
+++ 240911/중앙값 구하기/find-the-median.java
@@ -4,19 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
-        if(a < b) {
-            if(b < c) {
+        if (a < b) {
+            if (b < c) {
                 System.out.println(b);
             }
-            else {
+            // c < b > a
+            else if (a > c) {
+                System.out.println(a);
+            } else {
                 System.out.println(c);
             }
         }
         // b < a
         else {
-            if(a < c) {
+            if (a < c) {
                 System.out.println(a);
-            } else {
+            } else if (b > c) {
+                System.out.println(b);
+            }
+            // b < c
+            else {
                 System.out.println(c);
             }
         }

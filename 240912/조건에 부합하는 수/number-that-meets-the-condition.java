57672b37693ec3db27cfb88702665b6242aa2805
JeongHyeon Song <sjh00kr@naver.com>
2024-09-12 17:46:46 +0900

    Title: 조건에 부합하는 수, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240912/조건에 부합하는 수/number-that-meets-the-condition.java
+++ 240912/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -5,16 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         for (int i = 1; i <= a ; i++) {
-            if(i % 2 == 0){
+            if(i % 2 == 0 && i % 4 != 0){
                 continue;
             }
-            if(i / 8 % 2 == 0) {
+            if((i / 8) % 2 == 0) {
                 continue;
             }
             if (i % 7 < 4) {
                 continue;
             }
-            System.out.print(i + " ");
+            System.out.println(i);
         }
     }
 }

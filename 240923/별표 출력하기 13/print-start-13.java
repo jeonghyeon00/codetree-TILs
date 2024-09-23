bcbc677e0eb7cdc75c4ad85230769243611f3728
JeongHyeon Song <sjh00kr@naver.com>
2024-09-23 14:25:45 +0900

    Title: 별표 출력하기 13, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240923/별표 출력하기 13/print-start-13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n - j; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+            for (int i = 0; i <= j; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

97fe316d47b0aa673cdc321e8e9e45b754b9e7eb
JeongHyeon Song <sjh00kr@naver.com>
2024-09-26 13:51:23 +0900

    Title: 행에 따라 다르게 출력, Time: 229ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240926/행에 따라 다르게 출력/output-defferently-each-row.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int outputNumber = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i % 2 == 0) {
+                    outputNumber++;
+                } else {
+                    outputNumber+= 2;
+                }
+                System.out.print(outputNumber + " ");
+            }
+            System.out.println();
+        }
+    }
+}

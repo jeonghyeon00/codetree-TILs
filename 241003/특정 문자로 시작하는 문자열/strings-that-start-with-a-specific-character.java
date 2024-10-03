cc21e5b1f181f6155cbeb9aab66da834f9d0ca8f
JeongHyeon Song <sjh00kr@naver.com>
2024-10-03 09:55:31 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241003/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
+++ 241003/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
@@ -8,9 +8,9 @@ public class Main {
         int sum = 0;
         int count = 0;
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextLine();
+            arr[i] = sc.next();
         }
-        String alphabet = sc.nextLine();
+        String alphabet = sc.next();
         for (int i = 0; i < n; i++) {
             if(arr[i].charAt(0) == alphabet.charAt(0)){
                 sum += arr[i].length();

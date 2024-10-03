5b1a8721ad3a82f3297339a638ef617f517de4d0
JeongHyeon Song <sjh00kr@naver.com>
2024-10-03 09:52:40 +0900

    Title: 특정 문자로 시작하는 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241003/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLine();
+        }
+        String alphabet = sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            if(arr[i].charAt(0) == alphabet.charAt(0)){
+                sum += arr[i].length();
+                count++;
+            }
+        }
+        System.out.printf("%d %.2f", count, (double)sum/count);
+    }
+}

45ee9ddb015f384c0421c99955c791e8ff73b2e4
JeongHyeon Song <sjh00kr@naver.com>
2024-10-04 10:36:55 +0900

    Title: 두 정수에 대한 연산값 2, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241004/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
+++ 241004/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -2,11 +2,26 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int maxMultiply(int a, int b) {
+        return Math.max(a, b) * 2;
+    }
+
+    public static int minAdd(int a, int b) {
+        return Math.min(a, b) + 10;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-        System.out.printf("%d %d", Math.min(a, b) + 10, Math.max(a, b) * 2);
+        if (a > b) {
+            a = maxMultiply(a, b);
+            b = minAdd(a, b);
+        } else {
+            b = minAdd(a, b);
+            a = maxMultiply(a, b);
+        }
+        
+        System.out.printf("%d %d", a, b);
     }
 }

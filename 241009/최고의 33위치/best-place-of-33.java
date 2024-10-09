2fa78ccf3ab6c304fba959c095fab92259a2e4cd
JeongHyeon Song <sjh00kr@naver.com>
2024-10-09 11:56:43 +0900

    Title: 최고의 33위치, Time: 174ms, Memory: 11MB, Status: Passed - CodeTree

--- 241009/최고의 33위치/best-place-of-33.java
+++ 241009/최고의 33위치/best-place-of-33.java
@@ -1,22 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
+    static int[][] matrix;
+
+    public static int getCoins(int startRow, int startCol) {
+        int coinCount = 0;
+        for (int row = startRow; row < startRow + 3; row++) {
+            for (int col = startCol; col < startCol + 3; col++) {
+                if(matrix[row][col] == 1) {
+                    coinCount++;
+                }
+            }
+        }
+        return coinCount;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int count = 0;
-        int[][] matrix = new int[n][n];
+        matrix = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(matrix[i][j] == 1) {
-                    count++;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if(row + 2 >= n || col + 2 >= n) {
+                    continue;
                 }
+                count = Math.max(count, getCoins(row, col));
             }
         }
         System.out.println(count);

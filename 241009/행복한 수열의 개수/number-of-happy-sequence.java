c5269485492450b40d0bdfbe6c34f7bf77fedde2
JeongHyeon Song <sjh00kr@naver.com>
2024-10-09 21:01:44 +0900

    Title: 행복한 수열의 개수, Time: 310ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 241009/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    static int[][] matrix;
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // 연속해야하는 숫자의 개수
+        int m = sc.nextInt();
+        int count = 0;
+
+        matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int lastNumber;
+        int sameNumberCount = 0;
+        // 가로 탐색
+        for (int row = 0; row < n; row++) {
+            lastNumber = matrix[row][0];
+            for (int col = 0; col < n; col++) {
+                if(lastNumber == matrix[row][col]) {
+                    sameNumberCount++;
+                } else {
+                    lastNumber = matrix[row][col];
+                    sameNumberCount = 1;
+                }
+            }
+            if(sameNumberCount >= m) {
+                count++;
+            }
+        }
+
+        // 세로 탐색
+        for (int col = 0; col < n; col++) {
+            lastNumber = matrix[0][col];
+            for (int row = 0; row < n; row++) {
+                if(lastNumber == matrix[row][col]) {
+                    sameNumberCount++;
+                } else {
+                    lastNumber = matrix[row][col];
+                    sameNumberCount = 1;
+                }
+            }
+            if(sameNumberCount >= m) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}

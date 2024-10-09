ffb60aa0ea1ee9677d14225151cf2595612afbb5
JeongHyeon Song <sjh00kr@naver.com>
2024-10-09 11:47:44 +0900

    Title: 최고의 33위치, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241009/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(matrix[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}

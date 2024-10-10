8f52dcce18acc6776fdcbc9886e4191454f55ed0
JeongHyeon Song <sjh00kr@naver.com>
2024-10-10 17:05:23 +0900

    Title: 그래프 탐색, Time: 491ms, Memory: 28MB, Status: Passed - CodeTree

--- 241010/그래프 탐색/graph-traversal.java
+++ 241010/그래프 탐색/graph-traversal.java
@@ -4,10 +4,10 @@ public class Main {
     public static int[][] grid;
     public static boolean[] visited;
     // vertex count
-    public static int m;
+    public static int n;
     public static int count = 0;
     public static void DFS(int vertex) {
-        for (int currentVertex = 1; currentVertex <= m; currentVertex++) {
+        for (int currentVertex = 1; currentVertex <= n; currentVertex++) {
             if (grid[vertex][currentVertex] == 1 && !visited[currentVertex]) {
                 visited[currentVertex] = true;
                 count++;
@@ -18,17 +18,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        m = sc.nextInt();
+        n = sc.nextInt();
+        int m = sc.nextInt();
 
         grid = new int[n + 1][n + 1];
-        visited = new boolean[m + 1];
+        visited = new boolean[n + 1];
         for (int i = 0; i < m; i++) {
             int col = sc.nextInt();
             int row = sc.nextInt();
             grid[row][col] = 1;
             grid[col][row] = 1;
         }
+
         visited[1] = true;
         DFS(1);
         System.out.println(count);

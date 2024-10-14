70fe147bfb12ea3be1450d77c58b4d88acc1d3ac
JeongHyeon Song <sjh00kr@naver.com>
2024-10-14 16:17:05 +0900

    Title: 뿌요뿌요, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241014/뿌요뿌요/puyo-puyo.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static int[][] grid;
+    public static boolean[][] visited;
+    public static int n;
+    public static int blockCount;
+
+    public static void dfs(int x, int y, int k) {
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n) {
+                continue;
+            }
+
+            if (visited[nx][ny] || grid[nx][ny] == 0 || grid[nx][ny] != k) {
+                continue;
+            }
+
+            visited[nx][ny] = true;
+            blockCount++;
+            dfs(nx, ny, k);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int maxBlockCount = 0;
+        int blockExplodeNumber = 0;
+        grid = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int k = 1; k <= 100; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    blockCount = 0;
+                    visited = new boolean[n][n];
+                    if (grid[i][j] == k) {
+                        visited[i][j] = true;
+                        blockCount = 1;
+                        dfs(i, j, k);
+                        if (blockCount >= 4) {
+                            maxBlockCount = blockCount;
+                            blockExplodeNumber = grid[i][j];
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(blockExplodeNumber + " " + maxBlockCount);
+    }
+}

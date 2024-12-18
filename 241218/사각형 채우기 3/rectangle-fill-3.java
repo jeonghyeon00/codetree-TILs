34d314b20ee6282b62815f0fb599bda4f4e79f33
JeongHyeon Song <sjh00kr@naver.com>
2024-12-18 11:50:10 +0900

    Title: 사각형 채우기 3, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241218/사각형 채우기 3/rectangle-fill-3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static int[] dp = new int[1001];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        dp[0] = 1;
+        dp[1] = 2;
+        dp[2] = 7;
+
+        int n = sc.nextInt();
+        for(int i = 3; i<= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+        System.out.print(dp[n]);
+    }
+}
